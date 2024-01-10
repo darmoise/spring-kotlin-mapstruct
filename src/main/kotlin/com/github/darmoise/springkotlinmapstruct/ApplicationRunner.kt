3e47c3e32808832cf8e47a9264ec51a3f26f282f
darmoise <darmoise@localhost>
2024-01-10 15:50:21 +0300

    first commit

---
+++ src/main/kotlin/com/github/darmoise/springkotlinmapstruct/ApplicationRunner.kt
@@ -0,0 +1,11 @@
+package com.github.darmoise.springkotlinmapstruct
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinMapstructApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinMapstructApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/darmoise/springkotlinmapstruct/SpringKotlinMapstructApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.darmoise.springkotlinmapstruct
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinMapstructApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
